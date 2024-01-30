14164a4561cb1f31f080862430dc141caf863e7c
Nim <94055511+Mimoletteleplusbeau@users.noreply.github.com>
2024-01-30 17:14:52 +0100

    Update Cam Proto + test snow

--- Honk/Assets/---Scripts---/Tech/CameraTrigger.cs
+++ Honk/Assets/---Scripts---/Tech/CameraTrigger.cs
@@ -5,20 +5,48 @@ using Cinemachine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera Cam1, Cam2, Cam3;
+    [SerializeField] private CinemachineVirtualCamera Cam1, Cam2, Cam3, Cam4;
+    [SerializeField] private Collider Collider2, Collider3, Collider4;
     [SerializeField] private CinemachineBrain Brain;
-    //[SerializeField] private Collider collider;
-
 
     private void OnTriggerEnter(Collider other)
     {
-            Debug.Log("F");
+        if (other == Collider2)
+        {
+            Debug.Log("InC2");
             Cam2.gameObject.SetActive(true);
+        }   
+        else if (other == Collider3)
+        {
+            Debug.Log("InC3");
+            Cam3.gameObject.SetActive(true);
+        }
+        else if (other == Collider4)
+        {
+            Debug.Log("InC4");
+            Cam4.gameObject.SetActive(true);
+        }
+        
     }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Off");
-        Cam2.gameObject.SetActive(false);
+        if (other == Collider2)
+        {
+            Debug.Log("OutC2");
+            Cam2.gameObject.SetActive(false);
+        }
+        else if (other == Collider3)
+        {
+            Debug.Log("OutC3");
+            Cam3.gameObject.SetActive(false);
+        }
+        else if (other == Collider4)
+        {
+            Debug.Log("OutC4");
+            Cam4.gameObject.SetActive(false);
+        }
     }
+
+
 }
     
