3497c890bb782242dca8037ce29fd48517177247
Nim <94055511+Mimoletteleplusbeau@users.noreply.github.com>
2024-01-30 14:14:57 +0100

    Cinemachine ++

--- Honk/Assets/---Scripts---/Tech/CameraTrigger.cs
+++ Honk/Assets/---Scripts---/Tech/CameraTrigger.cs
@@ -7,26 +7,18 @@ public class CameraTrigger : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera Cam1, Cam2, Cam3;
     [SerializeField] private CinemachineBrain Brain;
-    [SerializeField] private Collider collider;
-    bool tt = true;
+    //[SerializeField] private Collider collider;
+
 
     private void OnTriggerEnter(Collider other)
     {
-
-        Debug.Log("trigger");
-        if (tt == true)
-        {
-            Debug.Log("T");
-            Cam2.gameObject.SetActive(true);
-            tt = false;
-        }
-        else
-        {
             Debug.Log("F");
-            Cam2.gameObject.SetActive(false);
-            tt = true;
-        }
-
+            Cam2.gameObject.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Off");
+        Cam2.gameObject.SetActive(false);
     }
 }
     
