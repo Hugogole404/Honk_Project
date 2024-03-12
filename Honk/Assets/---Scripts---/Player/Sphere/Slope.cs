18696d76f073eb60168d03beb18d6927a0695040
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-03-12 14:08:29 +0100

    boule

--- Honk/Assets/---Scripts---/Player/Sphere/Slope.cs
+++ Honk/Assets/---Scripts---/Player/Sphere/Slope.cs
@@ -5,29 +5,25 @@ using UnityEngine.InputSystem;
 public class Slope : MonoBehaviour
 {
     public bool IsGrounded = false;
+    public bool CanSpeedDown = false;
     [SerializeField] private float _speed;
+    [SerializeField] private float _speedDownValue;
     [SerializeField] private float _boostSpeedStartSlope;
     [SerializeField] private float _maxSpeed;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _maxTimerJump;
+    [SerializeField] private float _gravityMultiplier;
+    [SerializeField] private bool _modWalk = true;
+    [SerializeField] private bool _modSlide = false;
+    private float _gravity = -9.81f;
     private float _currentTimerJump;
     private bool _canJump;
-    private bool _modWalk;
-    private bool _modSlide;
     private Vector2 _moveInput;
     private Rigidbody _rigidbody;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         _moveInput = context.ReadValue<Vector2>();
-        if (_modWalk)
-        {
-            _rigidbody.AddForce(new Vector3(_moveInput.x, 0, _moveInput.y) * _speed, ForceMode.Force);
-        }
-        if (_modSlide)
-        {
-            _rigidbody.AddForce(new Vector3(_moveInput.x, 0, _moveInput.y) * _speed, ForceMode.Force);
-        }
     }
     public void StartSlide(InputAction.CallbackContext context)
     {
@@ -52,6 +48,21 @@ public class Slope : MonoBehaviour
             _currentTimerJump = 0;
         }
     }
+    private void SpeedDown()
+    {
+        //_rigidbody.velocity.sqrMagnitude
+    }
+    private void ApplyMovement()
+    {
+        if (_modWalk)
+        {
+            _rigidbody.AddForce(new Vector3(_moveInput.x, 0, _moveInput.y) * _speed, ForceMode.Force);
+        }
+        if (_modSlide)
+        {
+            _rigidbody.AddForce(new Vector3(_moveInput.x, 0, _moveInput.y) * _speed, ForceMode.Force);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -67,6 +78,7 @@ public class Slope : MonoBehaviour
             IsGrounded = false;
         }
     }
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -82,6 +94,8 @@ public class Slope : MonoBehaviour
         {
             _canJump = true;
         }
+        ApplyMovement();
+        _rigidbody.AddForce(Vector3.down * _gravity * Time.deltaTime * _gravityMultiplier);
     }
     private void FixedUpdate()
     {
