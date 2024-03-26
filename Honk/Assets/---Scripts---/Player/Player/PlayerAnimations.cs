79bc964b3373fa14761ccc87d617a80d02a081a4
AdamZbuub <107420297+AdamZbuub@users.noreply.github.com>
2024-03-26 17:11:47 +0100

    JE pris

--- Honk/Assets/---Scripts---/Player/Player/PlayerAnimations.cs
+++ Honk/Assets/---Scripts---/Player/Player/PlayerAnimations.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public CharacterController CharaController;
 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
     // Update is called once per frame
+
+    public Animator m_Animator;
+
     void Update()
     {
-        
+        //A mettre dans l'update de base
+        if (CharaController.isGrounded == true && m_Animator.GetBool("isJumping") == true)
+        {
+            m_Animator.SetBool("isJumping", false);
+        }
+
+        //A mettre au moment de l'input du jump
+        m_Animator.SetBool("isJumping", true);
+
+        // A mettre au moment de l'input de marche
+        m_Animator.SetBool("isMoving", true);
+
+        // A mettre au moment de l'arret de l'input de marche
+        m_Animator.SetBool("isMoving", false);
+
+
+
     }
+
+
 }
