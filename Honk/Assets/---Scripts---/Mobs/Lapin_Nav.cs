0144d552cbb26fa8725b522c9e790c13239a461d
AdamZbuub <107420297+AdamZbuub@users.noreply.github.com>
2024-04-05 20:28:19 +0200

    NavMeshRAbbit

---
+++ Honk/Assets/---Scripts---/Mobs/Lapin_Nav.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation.Editor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Lapin_Nav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(CheckTransform());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator CheckTransform()
+    {
+        Debug.Log("commence");
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("fini");
+        if (Vector3.Distance(agent.transform.position, player.position) <= 2)
+        {
+            Vector3 dirToPlayer = transform.position - player.position;
+            Vector3 newPos = transform.position + dirToPlayer *6;
+            agent.destination = newPos;
+            //StartCoroutine(JustRan());
+            //StopCoroutine(CheckTransform());
+        }
+        else if (Vector3.Distance(agent.transform.position, player.position) >= 3)
+        {
+            agent.destination = player.position;
+
+        }
+        else
+        {
+            agent.destination = agent.transform.position;
+        }
+
+
+        StartCoroutine(CheckTransform());
+    }
+    //IEnumerator JustRan()
+    //{
+    //    StopCoroutine(CheckTransform());
+    //    StartCoroutine(CheckAlentours());
+    //    yield return new WaitForSeconds(3f);
+    //    StartCoroutine(CheckTransform());
+    //    StopCoroutine(CheckAlentours());
+    //}
+}
