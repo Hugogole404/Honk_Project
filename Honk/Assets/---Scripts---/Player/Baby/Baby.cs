085d3869dcb1fc5e148ffdd680fe92dd2016302c
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-04-11 17:25:33 +0200

    Commencement du deplacement du petit

--- Honk/Assets/---Scripts---/Player/Baby/Baby.cs
+++ Honk/Assets/---Scripts---/Player/Baby/Baby.cs
@@ -1,13 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Baby : MonoBehaviour
 {
+    private Transform ToFollow;
+
+    [SerializeField] private Vector3 _offset;
     private HoldBaby _holdBaby;
+
+    void Start()
+    {
+        _offset = ToFollow.position - transform.position;
+    }
+    private void MoveBaby()
+    {
+        // pas encore operationnel
+        if(_holdBaby.IsOnHisBack == false)
+        {
+            transform.position = ToFollow.position - _offset;
+            transform.rotation = ToFollow.rotation;
+        }
+    }
 
     private void Awake()
     {
         _holdBaby = FindAnyObjectByType<HoldBaby>();
+        ToFollow = FindAnyObjectByType<PlayerMovements>().transform;
+    }
+    private void Update()
+    {
+        MoveBaby();
     }
 }
