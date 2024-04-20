25ff583c2b0c2606df24d76f81cb895def049a6c
AdamZbuub <107420297+AdamZbuub@users.noreply.github.com>
2024-04-20 22:28:09 +0200

    Scenefinalavancement

---
+++ Honk/Assets/---Scripts---/Player/Player/FeetPlacement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FeetPlacement : MonoBehaviour
+{
+    public Object PasDumanchot;
+    public bool isSnow;
+    int layerMask = 1 << 11;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 2f, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+            isSnow = true;
+        }
+        else
+            isSnow = false;
+}
+
+    public void TriggerEvent()
+    {
+
+        if (isSnow)
+        {
+            Instantiate(PasDumanchot, gameObject.transform.position + new Vector3(0.4f, 0.2f, 0), gameObject.transform.rotation * Quaternion.Euler(Vector3.right * 90) * Quaternion.Euler(Vector3.forward));
+        }
+       
+    }
+    public void TriggerEvent2()
+    {
+        if (isSnow)
+        {
+            Instantiate(PasDumanchot, gameObject.transform.position + new Vector3(-0.4f, 0.2f, 0), gameObject.transform.rotation * Quaternion.Euler(Vector3.right * 90) * Quaternion.Euler(Vector3.forward));
+        }
+    }
+
+    public void AtterissageNeige()
+    {
+        if (isSnow)
+        {
+            Instantiate(PasDumanchot, gameObject.transform.position + new Vector3(-0.3f, 0.2f, 0), gameObject.transform.rotation * Quaternion.Euler(Vector3.right * 90) * Quaternion.Euler(Vector3.forward));
+            Instantiate(PasDumanchot, gameObject.transform.position + new Vector3(0.3f, 0.2f, 0), gameObject.transform.rotation * Quaternion.Euler(Vector3.right * 90) * Quaternion.Euler(Vector3.forward));
+        }
+    }
+}
