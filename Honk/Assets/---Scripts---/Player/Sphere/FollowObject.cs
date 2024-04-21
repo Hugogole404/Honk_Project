37bff536a6b803b6b7318d558bafe747ef28c59c
Nim <nimlot.elise@gmail.com>
2024-04-21 16:38:44 +0200

    Lighting cave

--- Honk/Assets/---Scripts---/Player/Sphere/FollowObject.cs
+++ Honk/Assets/---Scripts---/Player/Sphere/FollowObject.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FollowObject : MonoBehaviour
 {
     [SerializeField] GameObject _objectToFollow;
+    public float OffsetY = 0;
+    public float OffsetZ = 0;
 
     private void Update()
     {
         //GetComponent<CharacterController>().enabled = false;
-        transform.position = new Vector3(_objectToFollow.transform.position.x, _objectToFollow.transform.position.y + 0.5f, _objectToFollow.transform.position.z);
+        transform.position = new Vector3(_objectToFollow.transform.position.x , _objectToFollow.transform.position.y + OffsetY, _objectToFollow.transform.position.z + OffsetZ);
         //GetComponent<CharacterController>().enabled = true;
         //transform.eulerAngles = new Vector3(transform.eulerAngles.x, _objectToFollow.transform.eulerAngles.y, transform.eulerAngles.z);
     }
