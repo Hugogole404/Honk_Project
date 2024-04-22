ec3fd1e6f79280ffbc15084e31071780b91a96fb
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-04-22 21:44:32 +0200

    Zone de detection next scene

---
+++ Honk/Assets/---Scripts---/Area/AreaEndZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaEndZone : MonoBehaviour
+{
+    public string NameSceneToLoad;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerMovements>() != null)
+        {
+            SceneManager.LoadScene(NameSceneToLoad);
+        }
+    }
+}
