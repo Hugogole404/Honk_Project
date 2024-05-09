fcc6c8ba6a7fa877d891b2b67574181909d977fa
Nim <nimlot.elise@gmail.com>
2024-05-09 21:34:08 +0200

    Flower sans lag

---
+++ Honk/Assets/---Scripts---/Tech/EmissiveColider.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class EmissiveColider : MonoBehaviour
+{
+    private Material emissiveMaterial;
+    private float Time = 0;
+    private bool InOut = false;
+    public float EaseIn;
+    public float EaseOut;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        emissiveMaterial = rend.material;
+
+    }
+    private void Update()
+    {
+        if (InOut == true && Time<=1)
+        {
+            
+            Time += EaseIn;
+            
+            emissiveMaterial.SetColor("_EmissionColor", Color.white * Time);
+            rend.material = emissiveMaterial;
+            rend.material.EnableKeyword("_EMISSION");
+        }
+        else if (InOut == false && Time>0) 
+        {
+            Time -= EaseOut;
+            emissiveMaterial.SetColor("_EmissionColor", Color.white * Time);
+            rend.material = emissiveMaterial;
+            rend.material.EnableKeyword("_EMISSION");
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InOut = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InOut = false;
+    }
+}
