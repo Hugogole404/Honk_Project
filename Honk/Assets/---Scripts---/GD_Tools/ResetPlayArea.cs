0f38b3a5d3e4ebdaf1b9de8498fd2940c2cd9c6b
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-05-17 09:40:20 +0200

    Reset player (Pour la GD)

--- Honk/Assets/---Scripts---/GD_Tools/ResetPlayArea.cs
+++ Honk/Assets/---Scripts---/GD_Tools/ResetPlayArea.cs
@@ -58,7 +58,7 @@ public class ResetPlayArea : MonoBehaviour
         {
             if(PlayerHaveToBeReset)
             {
-                // reset player
+                _playerMovements.TeleportToSpawnPoint();
             }
             AreaReset();
         }
