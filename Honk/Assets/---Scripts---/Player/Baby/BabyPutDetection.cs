509698582dbeb47e1102a13c2e1a2272c94251a8
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-05-19 20:23:50 +0200

    Modif LD

--- Honk/Assets/---Scripts---/Player/Baby/BabyPutDetection.cs
+++ Honk/Assets/---Scripts---/Player/Baby/BabyPutDetection.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class BabyPutDetection : MonoBehaviour
 {
     public bool CanBePut;
-
+    public int NumberMinOfColliderToDetect;
     private void Update()
     {
         Collider[] hitCollider = Physics.OverlapSphere(transform.position, 0f);
-        if (hitCollider.Length == 2) CanBePut = true;
-        else if (hitCollider.Length >= 2) CanBePut = false;
+        if (hitCollider.Length == NumberMinOfColliderToDetect) CanBePut = true;
+        else if (hitCollider.Length >= NumberMinOfColliderToDetect) CanBePut = false;
         foreach (Collider col in hitCollider)
         {
             print(col.gameObject.name);
