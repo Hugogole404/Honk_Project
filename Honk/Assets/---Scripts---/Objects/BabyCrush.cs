ee51571a383213898ed0afcd75e5882bb7cf6520
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-05-20 19:51:38 +0200

    Avancée resolutions des bugs

--- Honk/Assets/---Scripts---/Objects/BabyCrush.cs
+++ Honk/Assets/---Scripts---/Objects/BabyCrush.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class BabyCrush : MonoBehaviour
 {
+    HoldBaby _holdBaby;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<TestBabyWalk>() != null)
         {
-            if (transform.parent.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 0.1f)
+            if (transform.parent.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 0.1f && _holdBaby.IsOnHisBack == false)
             {
-                Debug.Log("CRUSH"); ;
                 other.GetComponent<TestBabyWalk>().transform.localScale = new Vector3(other.GetComponent<TestBabyWalk>().transform.localScale.x,
                     other.GetComponent<TestBabyWalk>().ScaleCrushedBaby, other.GetComponent<TestBabyWalk>().transform.localScale.z);
             }
         }
+        //if (other.GetComponent<PushObstacles>() != null)
+        //{
+        //    other.GetComponent<PushObstacles>()._bloc.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        //    print(other.GetComponent<PushObstacles>()._bloc.GetComponent<Rigidbody>().gameObject.name);
+        //    GetComponentInParent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        //}
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<TestBabyWalk>() != null)
+        if (other.gameObject.GetComponent<TestBabyWalk>() != null /*&& _holdBaby.IsOnHisBack == false*/)
         {
             other.gameObject.GetComponent<TestBabyWalk>().CanGoToNormalScale = true;
         }
+    }
+
+    private void Awake()
+    {
+        _holdBaby = FindObjectOfType<HoldBaby>();
     }
 }
