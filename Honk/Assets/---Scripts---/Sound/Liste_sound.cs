5d4b2d381a884e90cd313d93813eaa64e0a59cb5
Sorako-Kun <lea-maria.ambrosio@lacatholille.fr>
2024-05-23 14:28:32 +0200

    List Sound debut script

---
+++ Honk/Assets/---Scripts---/Sound/Liste_sound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liste_sound : MonoBehaviour
+{
+    public AudioClip[] jump;
+    public AudioSource paw_snow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
