d9adeeb794d888246eca92a67fe5549e207f112b
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-05-23 22:12:06 +0200

    UI Zone + UI Fade

---
+++ Honk/Assets/---Scripts---/UI/AreaUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class AreaUI : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup UI_ToActivate_or_not;
+    [SerializeField] private float _fadeDuration;
+    private Tween _fadeTween;
+
+
+    private void Fade(float endVal, float duration, TweenCallback onEnd)
+    {
+        if (_fadeTween != null)
+        {
+            _fadeTween.Kill(false);
+        }
+        _fadeTween = UI_ToActivate_or_not.DOFade(endVal, duration);
+        _fadeTween.onComplete += onEnd;
+    }
+
+    private void FadeIn(float duration)
+    {
+        Fade(1f, duration, () => { UI_ToActivate_or_not.interactable = true; UI_ToActivate_or_not.blocksRaycasts = true; });
+    }
+    private void FadeOut(float duration)
+    {
+        Fade(0f, duration, () => { UI_ToActivate_or_not.interactable = false; UI_ToActivate_or_not.blocksRaycasts = false; });
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FadeIn(_fadeDuration);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        FadeOut(_fadeDuration);
+    }
+}
