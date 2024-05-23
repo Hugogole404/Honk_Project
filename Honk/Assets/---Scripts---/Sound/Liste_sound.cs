fbebf3391aae6c834d591ffd0ab72161760bf11a
Sorako-Kun <lea-maria.ambrosio@lacatholille.fr>
2024-05-23 15:17:15 +0200

    Liste son script fait

--- Honk/Assets/---Scripts---/Sound/Liste_sound.cs
+++ Honk/Assets/---Scripts---/Sound/Liste_sound.cs
@@ -6,6 +6,12 @@ public class Liste_sound : MonoBehaviour
 {
     public AudioClip[] jump;
     public AudioSource paw_snow;
+    public AudioSource paw_snow_grotte;
+    public AudioSource paw_grass;
+    public AudioSource paw_grass_grotte;
+    public AudioSource paw_grotte;
+    public AudioSource depot_petit;
+    public AudioSource depot_petit_grotte;
     void Start()
     {
         
