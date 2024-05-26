05d7b5933fb987aff4b1751ae9181b40ec6042a1
Nim <nimlot.elise@gmail.com>
2024-05-26 21:56:48 +0200

    Cave

--- Honk/Assets/---Scripts---/Player/Baby/BabyPutDetection.cs
+++ Honk/Assets/---Scripts---/Player/Baby/BabyPutDetection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BabyPutDetection : MonoBehaviour
@@ -11,9 +12,10 @@ public class BabyPutDetection : MonoBehaviour
         Collider[] hitCollider = Physics.OverlapSphere(transform.position, 0f);
         if (hitCollider.Length == NumberMinOfColliderToDetect) CanBePut = true;
         else if (hitCollider.Length >= NumberMinOfColliderToDetect) CanBePut = false;
-        //foreach (Collider col in hitCollider)
-        //{
-        //    print(col.gameObject.name);
-        //}
+        foreach (Collider col in hitCollider)
+        {
+            print(col.gameObject.name);
+            print(hitCollider.Length);
+        }
     }
 }
