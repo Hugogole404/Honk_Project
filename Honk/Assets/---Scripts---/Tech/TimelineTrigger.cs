e7eded551c25673997be658af3a57d00ecabf74f
Nim <94055511+Mimoletteleplusbeau@users.noreply.github.com>
2024-06-04 16:44:05 +0200

    ok orca qui marche pas trop

---
+++ Honk/Assets/---Scripts---/Tech/TimelineTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Vérifie si l'objet entrant est le joueur, ou ajoute une condition spécifique ici
+        if (other.CompareTag("Player"))
+        {
+            if (playableDirector != null)
+            {
+                Debug.Log("caca");
+                playableDirector.Play();
+            }
+            else
+            {
+                Debug.LogError("PlayableDirector is not assigned!");
+            }
+        }
+    }
+}
+
