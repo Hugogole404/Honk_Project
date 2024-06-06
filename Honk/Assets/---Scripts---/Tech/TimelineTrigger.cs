3566b1ba3f695e8d81a667281d4e80d86e78f015
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2024-06-06 15:18:21 +0200

    Normalement les timelines fonctionnent mais c'est a tester

--- Honk/Assets/---Scripts---/Tech/TimelineTrigger.cs
+++ Honk/Assets/---Scripts---/Tech/TimelineTrigger.cs
@@ -1,37 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class TimelineTrigger : MonoBehaviour
 {
     public PlayableDirector playableDirector;
+    [SerializeField] private PlayerMovements _playerMovements;
+    [SerializeField] private GameObject _spawnPoint;
+    private bool _canBePlay;
 
+    private void Start()
+    {
+        if (playableDirector != null)
+        {
+            _canBePlay = true;
+            playableDirector.stopped += OnPlayableDirectorStopped;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         // Vérifie si l'objet entrant est le joueur, ou ajoute une condition spécifique ici
-        if (other.CompareTag("Player"))
+        if (_canBePlay)
         {
-            if (playableDirector != null)
+            if (other.GetComponent<PlayerMovements>() != null)
             {
-                playableDirector.Play();
-                
+                if (playableDirector != null)
+                {
+                    _playerMovements.CanPlayerUseInputs = false;
+                    playableDirector.Play();
+                    _canBePlay = false;
+                }
+                else
+                {
+                    Debug.LogError("PlayableDirector is not assigned!");
+                }
             }
-            else
-            {
-                Debug.LogError("PlayableDirector is not assigned!");
-            }
-
         }
-
-
+    }
+    private void OnDestroy()
+    {
+        if (playableDirector != null)
+        {
+            playableDirector.stopped -= OnPlayableDirectorStopped;
+        }
     }
     void OnPlayableDirectorStopped(PlayableDirector director)
     {
         if (director == playableDirector)
         {
-            
+            _playerMovements.GetComponent<CharacterController>().enabled = false;
+            _playerMovements.transform.position = _spawnPoint.transform.position;
+            _playerMovements.GetComponent<CharacterController>().enabled = true;
+
+            _playerMovements.CanPlayerUseInputs = true;
         }
     }
 }
-
