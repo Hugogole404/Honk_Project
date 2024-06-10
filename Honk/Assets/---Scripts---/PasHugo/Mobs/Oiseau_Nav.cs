a734328677a01c414e576b759e64cbffea186444
AdamZbuub <107420297+AdamZbuub@users.noreply.github.com>
2024-06-10 16:31:43 +0200

    scenefinal01 qq changement

--- Honk/Assets/---Scripts---/PasHugo/Mobs/Oiseau_Nav.cs
+++ Honk/Assets/---Scripts---/PasHugo/Mobs/Oiseau_Nav.cs
@@ -23,49 +23,49 @@ public class Oiseau_Nav : MonoBehaviour
     {
         
         yield return new WaitForSeconds(0.1f);
-        
-        if (Vector3.Distance(agent.transform.position, player.position) <= 4 && contactNumber <= contactBefFly)
+        if (Vector3.Distance(agent.transform.position, player.position) <= 100)
         {
-            Vector3 dirToPlayer = transform.position - player.position;
-            Vector3 newPos = transform.position + dirToPlayer;
-            newPos += dirToPlayer;
-            newPos += dirToPlayer;
-            newPos += dirToPlayer;
-            newPos += dirToPlayer;
-            agent.destination = newPos;
-            //StartCoroutine(JustRan());
-            //StopCoroutine(CheckTransform());
-            m_animator.SetBool("IsMoving", true);
-            contactNumber++;
-            yield return new WaitForSeconds(0.5f);
-            StartCoroutine(CheckTransform());
-            yield break;
-        }
-        else if (Vector3.Distance(agent.transform.position, player.position) <= 4 && contactNumber > contactBefFly)
-        {
-            m_animator.SetTrigger("IsFlying");
-            yield return new WaitForSeconds(2f);
-            Destroy(gameObject);
-        }
-        else
-        {
-            int doImove = Random.Range(1, 10);
-            if(doImove == 1)
+            if (Vector3.Distance(agent.transform.position, player.position) <= 4 && contactNumber <= contactBefFly)
             {
-                agent.destination = agent.transform.position + new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+                Vector3 dirToPlayer = transform.position - player.position;
+                Vector3 newPos = transform.position + dirToPlayer;
+                newPos += dirToPlayer;
+                newPos += dirToPlayer;
+                newPos += dirToPlayer;
+                newPos += dirToPlayer;
+                agent.destination = newPos;
+                //StartCoroutine(JustRan());
+                //StopCoroutine(CheckTransform());
                 m_animator.SetBool("IsMoving", true);
-                yield return new WaitForSeconds(0.3f);
+                contactNumber++;
+                yield return new WaitForSeconds(0.5f);
                 StartCoroutine(CheckTransform());
                 yield break;
             }
+            else if (Vector3.Distance(agent.transform.position, player.position) <= 4 && contactNumber > contactBefFly)
+            {
+                m_animator.SetTrigger("IsFlying");
+                yield return new WaitForSeconds(2f);
+                Destroy(gameObject);
+            }
             else
             {
-                agent.destination = agent.transform.position;
-                m_animator.SetBool("IsMoving", false);
+                int doImove = Random.Range(1, 10);
+                if (doImove == 1)
+                {
+                    agent.destination = agent.transform.position + new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+                    m_animator.SetBool("IsMoving", true);
+                    yield return new WaitForSeconds(0.3f);
+                    StartCoroutine(CheckTransform());
+                    yield break;
+                }
+                else
+                {
+                    agent.destination = agent.transform.position;
+                    m_animator.SetBool("IsMoving", false);
+                }
             }
         }
-
-
         StartCoroutine(CheckTransform());
     }
 
